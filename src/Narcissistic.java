2e8d208860395f2f6965863a2e5938b19756fcce
Mark Andrey Acebu <markandrey.acebu@gmail.com>
2024-10-07 20:33:25 +0800

    [feat]: Add Narcissistic problem

---
+++ src/Narcissistic.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+public class Narcissistic {
+    public static void main(String[] args) {
+        System.out.println(isNarcissistic(153));        //true
+        System.out.println(isNarcissistic(1634));       //true
+        System.out.println(isNarcissistic(112));        //false
+    }
+
+    public static boolean isNarcissistic(int number) {
+        String stringNumber = String.valueOf(number);
+        char[] array = stringNumber.toCharArray();
+        int length = stringNumber.length();
+        int sum = 0;
+        for(int i = 0; i < length; i++){
+            sum += (int) Math.pow(Integer.parseInt(String.valueOf(array[i])), length);
+        }
+        return number == sum;
+    }
+}
+
+
