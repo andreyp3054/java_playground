00e7f0c88a2a1f0b4ea69225b44b90ace92f7f72
andreyp3054 <markandrey.acebu@gmail.com>
2025-03-22 07:01:40 +0800

    [feat]: Add Median problem

--- src/nationals/problems/Median.java
+++ src/nationals/problems/Median.java
@@ -1,28 +1,23 @@
 package nationals.problems;
 
 import java.util.*;
-
 public class Median {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String[] line = scanner.nextLine().split(" ");
-        float[] array = new float[line.length];
-        for(int i = 0; i < line.length; i++){
-            array[i] = Integer.parseInt(line[i]);
+        System.out.println("Enter a series of numbers");
+        String text = scanner.nextLine();
+        String[] array = text.split(" ");
+        int size = array.length;
+        double[] numbers = new double[size];
+        for (int i = 0; i < size; i++) {
+            numbers[i] = Double.parseDouble(array[i]);
         }
-
-        float median = 0;
-        if(array.length % 2 == 0){
-            int left = (array.length / 2) - 1;
-            System.out.println(left + " hey");
-            int right = (array.length/2);
-            System.out.println("right" + right);
-            median = (array[left] + array[right]) / 2;
+        double median = 0;
+        if (size % 2 == 0) {
+            median = (numbers[size / 2] + numbers[(size / 2) - 1]) / 2;
+        } else {
+            median = numbers[size / 2];
         }
-
-        System.out.println("median: " + median);
-
-
+        System.out.printf("%.0f", median);
     }
 }
